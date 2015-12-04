a096edb3ddeba448c6e659278fc2b91e12a2b9f5
Kaitlin Anderson <kaitlin_anderson@mac.com>
2015-12-03 20:29:08 -0700

    lists crud actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,45 @@
 class ItemsController < ApplicationController
   def show
+    @items = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path(item) 
+    end
   end
 
   private
     def item_params
+      params.require(:item).permit(:name, :description, :priority, :completed)
     end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,25 +5,46 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    @items = @list.items 
+    @items = @list.items
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
   def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    list = List.find(params[:id])
+    if list.destroy
+      redirect_to lists_path
+    else
+      redirect_to list_path(list)
+    end
   end
 
   private
     def list_params
+      params.require(:list).permit(:name)
     end
 end
